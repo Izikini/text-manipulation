410f5612eb975f34b2bb2479f22e082a73365066
Bohdan Bura <bohdan.bura@teb.edu.pl>
2025-12-16 11:34:32 +0100

    Zadanie 1.2

--- text-manipulation/Program.cs
+++ text-manipulation/Program.cs
@@ -2,6 +2,6 @@
 Console.WriteLine("Prosze o wpisanie dowolnego zdania");
 string sentence = Console.ReadLine();
 Console.WriteLine(sentence.Length + "- Dlugosc zdania\n");
-Console.WriteLine(sentence.ToUpper() + "Tekst sylko duzymi");
-Console.WriteLine(sentence.ToLower() + "Tekst sylko malymi");
+Console.WriteLine(sentence.ToUpper() + "- Tekst sylko duzymi");
+Console.WriteLine(sentence.ToLower() + "- Tekst sylko malymi");
 
