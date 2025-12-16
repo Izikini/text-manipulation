6d842ab0be67e59f631e9b1f46fb712ba4dfc4fd
4097747 <4097747@TE-GDA-D-D36G09>
2025-12-16 11:22:13 +0100

    Dodaj pliki projektów.

---
+++ text-manipulation/Program.cs
@@ -0,0 +1,7 @@
+﻿//Zadanie 1.1
+Console.WriteLine("Prosze o wpisanie dowolnego zdania");
+string sentence = Console.ReadLine();
+Console.WriteLine(sentence.Length + "- Dlugosc zdania\n");
+Console.WriteLine(sentence.ToUpper() + "Tekst sylko duzymi");
+Console.WriteLine(sentence.ToLower() + "Tekst sylko malymi");
+
