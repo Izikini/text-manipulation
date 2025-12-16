59a0565a8df58f502070b29832298226f03e4718
Bohdan Bura <bohdan.bura@teb.edu.pl>
2025-12-16 12:22:26 +0100

    4 zadanie

--- text-manipulation/Program.cs
+++ text-manipulation/Program.cs
@@ -1,16 +1,6 @@
 ﻿//Zadanie 3
 
-Console.WriteLine("Podaj tekst :");
-string tekst = Console.ReadLine();
-Console.WriteLine("Podaj znak do wyszukiwania");
-char znak = Console.ReadLine()[0];
-
-int wystapienia = 0;
-for(int i = 0; i < tekst.Length; i++)
-{
-    if (tekst[i] == znak)
-    {
-        wystapienia++;
-    }
-}
-Console.WriteLine($"Znak {znak} wystepuje w tekscie {wystapienia} razy.");
+Console.WriteLine("Podaj zdanie :");
+string sentence = Console.ReadLine();
+sentence = sentence.Replace(" ", "_");
+Console.WriteLine("Zdanie po zmianie spacji: "+sentence);
