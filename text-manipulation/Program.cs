973d6b735f56153b33f3d285be2ece79d46d9923
Bohdan <bgdnbura@gmail.com>
2025-12-20 16:35:18 +0100

    zadanie 6

--- text-manipulation/Program.cs
+++ text-manipulation/Program.cs
@@ -1,11 +1,24 @@
 ﻿//zadanie 5
+using System;
+using System.Diagnostics;
 using System.Globalization;
+using System.Runtime.ExceptionServices;
 
 Console.WriteLine("Podaj teks:");
-string tekst= Console.ReadLine();
+string tekst = Console.ReadLine().ToLower().Replace(" ","");
 
-for(int i = tekst.Length-1 ;i >=0 ;i--) {
+string naodwrot = "";
 
-    Console.Write(tekst[i]);
+for (int i = tekst.Length - 1; i >= 0; i--)
+{
+    naodwrot += tekst[i];
+}
 
+if (tekst == naodwrot)
+{
+    Console.WriteLine("Tekst jest palindromem");
+}
+else
+{
+    Console.WriteLine("Tekst nie jest palindromem");
 }
